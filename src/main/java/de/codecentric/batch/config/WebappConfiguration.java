165be319fbbe82f837955a5253e1b3e1563f22ae
Thomas Bosch <tbosch@gmx.de>
2014-08-01 12:35:21 +0200

    remove email

--- src/main/java/de/codecentric/batch/config/WebappConfiguration.java
+++ src/main/java/de/codecentric/batch/config/WebappConfiguration.java
@@ -21,7 +21,7 @@ import org.springframework.context.annotation.ImportResource;
 /**
  * Just the import of the webapp-config.xml of spring-batch-admin-manager.
  *
- * @author Thomas Bosch (tbosch@gmx.de)
+ * @author Thomas Bosch
  */
 @Configuration
 @ImportResource("classpath:/org/springframework/batch/admin/web/resources/webapp-config.xml")
